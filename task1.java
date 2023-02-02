50be4fc4cec40799ab888d942b6c7f0d2bfe1c94
Saglara <nohaevass@yandex.ru>
2023-02-02 20:33:48 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class task1 {
+    ArrayList <Integer> arList=new ArrayList<>();
+    LinkedList<Integer> linList= new LinkedList<>();  
     
-}
+} 
