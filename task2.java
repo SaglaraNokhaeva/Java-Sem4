a2a2a67684cf32188b881df3dd8455648d10b8fb
Saglara <nohaevass@yandex.ru>
2023-02-02 22:52:44 +0300

    Update task2.java

--- task2.java
+++ task2.java
@@ -8,17 +8,19 @@ import java.util.Scanner;
 public class task2 {
     public static void main(String[] args) {
 
-        LinkedList <String> text= new LinkedList<>();        
-        Scanner scan=new Scanner(System.in);
-       
-        while(true){
-            String input =scan.nextLine();
-            if(input.equals("stop")) break;
-            String [] str = input.split("~");
-            if (str[0].equals("print")) System.out.println(text.remove(Integer.parseInt(str[1])));
-            else text.add(Integer.parseInt(str[1]),str[0]);
+        LinkedList<String> text = new LinkedList<>();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("stop"))
+                break;
+            String[] str = input.split("~");
+            if (str[0].equals("print"))
+                System.out.println(text.remove(Integer.parseInt(str[1])));
+            else
+                text.add(Integer.parseInt(str[1]), str[0]);
         }
         scan.close();
-    }    
-    
+    }
 }
