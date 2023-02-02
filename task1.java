a02c82a3d2b5f5246134ad9de166ac6c0f856094
Saglara <nohaevass@yandex.ru>
2023-02-02 21:20:52 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -2,7 +2,25 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class task1 {
-    ArrayList <Integer> arList=new ArrayList<>();
-    LinkedList<Integer> linList= new LinkedList<>();  
-    
-} 
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arList = new ArrayList<>();
+        LinkedList<Integer> linList = new LinkedList<>();
+        System.out.println(System.currentTimeMillis());
+        long timeStart = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            arList.add(i);
+        }
+        long timeFinish = System.currentTimeMillis();
+
+        System.out.println(timeFinish - timeStart);
+        System.out.println("--------------------");
+
+        timeStart = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            linList.add(i);
+        }
+        timeFinish = System.currentTimeMillis();
+        System.out.println(timeFinish - timeStart);
+    }
+}
